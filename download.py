ba599c1ad6d813312dc7ba97fe5ec0f16e4d9359
quubble <45556712+quubble@users.noreply.github.com>
2018-12-11 20:40:28 +0530

    Add files via upload

---
+++ download.py
@@ -0,0 +1,22 @@
+# imported the requests library 
+import requests, os, sys
+
+os.chdir("G:\Chanakya-TV-Series\YouTube-Windows-Powershell-Tutorials")
+ 
+image_url = "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+
+# URL of the image to be downloaded is defined as image_url 
+r = requests.get(image_url, allow_redirects=True) # create HTTP response object 
+
+# send a HTTP request to the server and save 
+# the HTTP response in a response object called r 
+with open("python_logo.png",'wb') as f: 
+
+	# Saving received content as a png file in 
+	# binary format 
+
+	# write the contents of the response (r.content) 
+	# to a new file in binary mode. 
+	f.write(r.content)
+
+	
